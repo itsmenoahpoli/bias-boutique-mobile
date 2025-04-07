2ac60093fc0067a51c7d7a3ac06fa43c71c38d4b
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2025-04-07 18:11:50 +0800

    temp disabled expo_public

--- constants/settings.constant.ts
+++ constants/settings.constant.ts
@@ -4,16 +4,15 @@ const checkEnvironment = (env: AppEnvironment) => {
   return env === CURRENT_APP_ENV;
 };
 
-const CURRENT_APP_ENV: AppEnvironment = process.env
-  .EXPO_PUBLIC_ENV as AppEnvironment;
+const CURRENT_APP_ENV: AppEnvironment = "DEV";
 
-const APP_API_URL: string = process.env.EXPO_PUBLIC_API_URL as string;
+const APP_API_URL: string = "http://localhost:8000/api/v1";
 
 const IS_DEV: boolean = checkEnvironment(AppEnvironments.DEV);
 const IS_QA: boolean = checkEnvironment(AppEnvironments.QA);
 const IS_PROD: boolean = checkEnvironment(AppEnvironments.PROD);
 
-const SECRET_PASSKEY: string = process.env.EXPO_PUBLIC_SECRET_PASSKEY as string;
+const SECRET_PASSKEY: string = "SECRET";
 const ENCRYPTION_CONFIG = { mode: "cbc", ks: 256 } as any;
 
 export const SETTINGS = {
